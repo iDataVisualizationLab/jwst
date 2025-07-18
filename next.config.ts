aff372e1c8b86b1bf2e8dd6be5b992e5fc7ce55b
rinriko <rinrikosung@hotmail.com>
2025-07-18 07:27:33 -0500

    add more config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,53 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProduction = process.env.NEXT_PUBLIC_DEPLOYMENT === 'PRODUCTION';
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
-const nextConfig: NextConfig = {
+// Default values
+let assetPrefix = '';
+let basePath = '';
+
+if (isProduction || isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY ?.replace(/.*?\//, '') || process.env.NEXT_PUBLIC_REPO_NAME;
+
+  // // Get the version from environment variable (set in GitHub Actions)
+  // const version = process.env.NEXT_PUBLIC_VERSION || 'latest';
+
+  // // Ensure correct paths for each version
+  // assetPrefix = `/${repo}/${version}/`;
+  // basePath = `/${repo}/${version}`;
+  assetPrefix = `/${repo}`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  reactStrictMode: true,
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   trailingSlash: true,
+  env: {
+    BASE_PATH: basePath,
+  },
+
+  async headers() {
+    return [{
+      source: '/(.*)',
+      headers: [{
+        key: 'Access-Control-Allow-Origin',
+        value: '*',
+      }, ],
+    }, ];
+  },
+  async redirects() {
+    return [{
+      source: '/home',
+      destination: '/',
+      permanent: true,
+    }, ];
+  },
 };
 
 export default nextConfig;
